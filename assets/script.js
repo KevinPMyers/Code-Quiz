4905bb84da4bda7a243f9ce92ddc8d60d20a4f31
KevinPMyers <kmyers25@gmail.com>
2021-06-20 23:21:29 -0400

    Some style, registers clicks for questions, doesnt cycle questions again

--- assets/script.js
+++ assets/script.js
@@ -23,40 +23,40 @@ var buttonD = document.getElementById("d-btn");
 var codeQuestions = [
     {
     question: "Which language makes a website 'do things'?",
-    chooseA: "CSS",
-    chooseB: "HTML",
-    chooseC: "JavaScript",
-    chooseD: "Spanish",
+    chooseA: "A:CSS",
+    chooseB: "B:HTML",
+    chooseC: "C:JavaScript",
+    chooseD: "D:Spanish",
     rightAnswer: "C" },
     {
         question: "Where in the html would you link the CSS page?",
-        chooseA: "At the top of the page",
-        chooseB: "At the bottom of the head element",
-        chooseC: "The main section",
-        chooseD: "At the bottom of the body",
+        chooseA: "A:At the top of the page",
+        chooseB: "B:At the bottom of the head element",
+        chooseC: "C:The main section",
+        chooseD: "D:At the bottom of the body",
         rightAnswer: "B",
     },
     {
         question: "What is an example of a third party API?",
-        chooseA: "JQuery",
-        chooseB: "JavaScript",
-        chooseC: "Python",
-        chooseD: "A Pizza Interaction",
+        chooseA: "A:JQuery",
+        chooseB: "B:JavaScript",
+        chooseC: "C:Python",
+        chooseD: "D:A Pizza Interaction",
         rightAnswer: "A",
     },
     {
         question: "Which of these are primitive values?",
-        chooseA: "String",
-        chooseB: "Boolean",
-        chooseC: "Null",
-        chooseD: "All of these",
+        chooseA: "A:String",
+        chooseB: "B:Boolean",
+        chooseC: "C:Null",
+        chooseD: "D:All of these",
         rightAnswer: "D",
     },
 
 ];
 var lastQuestion = codeQuestions.length
 var currentQuestionArray = 0;
-var score = 0;
+var score = [0];
 console.log(score);
 timeRemain = 100;
 var correct;
@@ -68,7 +68,7 @@ function makeQuiz() {
         return newScore();
     }
     var currentQuestion = codeQuestions[currentQuestionArray];
-
+    
     quizEl.innerHTML = "<p>" + currentQuestion.question + "</p>";
 
     buttonA.innerHTML = currentQuestion.chooseA;
@@ -95,9 +95,9 @@ timeSet = setInterval(function() {
 
 };
 
-// function newScore(){
-
-// }
+ function newScore(){
+console.log(score);
+ }
 
 
 function checkRight(rightAnswer){
@@ -111,7 +111,7 @@ function checkRight(rightAnswer){
     }
 
     else if (rightAnswer !== correct && currentQuestionArray !== lastQuestion ){
-        alert("Nope nope nope!")
+        alert("Nope nope nope!");
         currentQuestionArray++;
         
         makeQuiz();
@@ -122,9 +122,17 @@ function checkRight(rightAnswer){
 }
 makeQuiz();
 
-buttonA.addEventListener("click", checkRight);
-buttonB.addEventListener("click", checkRight);
-buttonC.addEventListener("click", checkRight);
-buttonD.addEventListener("click", checkRight);
+buttonA.addEventListener("click", () => {
+    
+    checkRight;})
+buttonB.addEventListener("click", () => {
+    
+    checkRight;});
+buttonC.addEventListener("click", () => {
+    
+    checkRight;})
+buttonD.addEventListener("click", () => {
+    
+    checkRight;})
 
 startButton.addEventListener("click", startQuiz);
